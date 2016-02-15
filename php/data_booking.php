3bbfd3d072ed9378e07c552eb5e67f3f8e18d528
13513086@std <13513086@std.stei.itb.ac.id>
2016-02-15 15:32:23 +0700

    perbaikan data_booking

--- php/data_booking.php
+++ php/data_booking.php
@@ -18,6 +18,7 @@
     	die('Could not query:' . mysql_error());
 	}	
 
+	echo $user_name;
 	/* Cek Stok */
 	if($stok_atk == 0) {
 		$message = "Out of order";
@@ -29,16 +30,29 @@
 		/* ID USser */
 		$sql_user = "SELECT `ID_User` FROM `t_user` WHERE (`Nama_User` = '$user_name')";
 		$result_user = mysql_query($sql_user);
-		$id_user = mysql_result($result_user, 0);
+		//var_dump($result_user);
 		if (!$result_user) {
 	    	die('Could not query:' . mysql_error());
 		}	
+		$id_user = mysql_result($result_user, 0);
+		if (!$id_user) {
+			$sql_new_user = "INSERT INTO `t_user`(`Nama_User`) VALUES ('$user_name')";
+			mysql_query($sql_new_user);	
+			$sql_new_id = "SELECT `ID_User` FROM `t_user` WHERE (`Nama_User` = '$user_name')";
+			$result_id_user = mysql_query($sql_user);
+			if (!$result_id_user) {
+	    		die('Could not query:' . mysql_error());
+	    	}
+	    	$id_user = mysql_result($result_id_user, 0);
+			//echo $id_user;
+		}
 		$sql = "INSERT INTO `t_pemesanan`(`Tgl_Pemesanan`, `Jumlah`, `Tgl_Pengambilan`, `ID_ATK`, `ID_User`) VALUES (now(), '$jumlah', '$date', '$atk', '$id_user')";
 		mysql_query($sql);
 		$sql_atk = "UPDATE `t_atk` SET `Stok_ATK` = `Stok_ATK` - '$jumlah' WHERE (`ID_ATK` = '$atk')";
 		mysql_query($sql_atk);
-		header("Location: /ATK/booking.html");
+		header("Location: /ATK_Test/booking.html");
 	}
+	//header("Location: /ATK_Test/booking.html");
 	mysql_close();
 	
 ?>
