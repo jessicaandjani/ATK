29bb901be6f7b4b5c0320a8dd6d5cde07723d2aa
13513086@std <13513086@std.stei.itb.ac.id>
2016-02-17 06:05:36 +0700

    alert data booking

--- php/data_booking.php
+++ php/data_booking.php
@@ -39,42 +39,58 @@
 		die('Could not query:' . mysql_error());
 	}
 	$id_pemesanan = mysql_result($result_ID, 0);
-	/* Stok ATK */
+	
 	$x = 0;
-	foreach($atk as $value){
-		echo $value;
+	$habis = false;
+	$sisa = false;
+	while (($x < count($atk)) && (!$habis) && (!$sisa)) {
+		$value = $atk[$x];
+		/* Stok ATK */
 		$sql_stok = "SELECT `Stok_ATK` FROM `t_atk` WHERE (`ID_ATK` = '$value')";
 		$result_stok = mysql_query($sql_stok);
-		$stok_atk = mysql_result($result_stok, 0);
 		if (!$result_stok) {
 			die('Could not query:' . mysql_error());
 		}	
+		$stok_atk = mysql_result($result_stok, 0);
+
 		/* Cek Stok */
 		if($stok_atk == 0) {
-			$message = "Out of order";
-			echo("<script type='text/javascript'>
-					alert('$message');
-					window.history.back();
-				  </script>");
+			$habis = true;
+			$sql_atk_habis = "SELECT `Jenis_ATK` FROM `t_atk` WHERE (`ID_ATK` = '$value')";
+			$result_atk_habis = mysql_query($sql_atk_habis);
+			$atk_habis = mysql_result($result_atk_habis, 0);
 		} else if ($stok_atk < $jumlah[$x]) {
-			$message = "Only available " . $stok_atk;
-			echo("<script type='text/javascript'>
-					alert('$message');
-					window.history.back();
-				  </script>");
+			$sisa = true;
+			$sql_atk_sisa = "SELECT `Jenis_ATK` FROM `t_atk` WHERE (`ID_ATK` = '$value')";
+			$result_atk_sisa = mysql_query($sql_atk_sisa);
+			$atk_sisa = mysql_result($result_atk_sisa, 0);
 		} else {
 			$sql = "INSERT INTO `t_pesanan`(`ID_Pemesanan`, `ID_ATK`, `Jumlah`) VALUES ('$id_pemesanan', '$value', '$jumlah[$x]')";
 			mysql_query($sql);
 			$sql_atk = "UPDATE `t_atk` SET `Stok_ATK` = `Stok_ATK` - '$jumlah[$x]' WHERE (`ID_ATK` = '$value')";
 			mysql_query($sql_atk);
 			$x++;
-			$message = "Booking success! Thank you";
-			echo("<script type='text/javascript'>
-					alert('$message');
-					window.location.href='/atk/booking.php';
-				  </script>");
 		}
 	}
+	if($habis == true) {
+		$message = "Out of order " . $atk_habis;
+		echo("<script type='text/javascript'> 
+				alert('$message');
+				window.history.back();
+			  </script>");
+	} else if ($sisa == true) {
+		$message = $atk_sisa . " Only available " . $stok_atk;
+		echo("<script type='text/javascript'>
+				alert('$message');
+				window.history.back();
+			 </script>");
+
+	} else {
+		$message = "Success! Thank you";
+		echo("<script type='text/javascript'>
+				alert('$message');
+			    window.location.href='/atk/usage.html';
+			  </script>");
+	}
 	mysql_close();
-	
 ?>
